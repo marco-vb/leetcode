d13744c3f6a6b301172c4eb028f24f0420e0b4f9
marco-vb <marcofilipe03@hotmail.com>
2024-01-08 15:25:47 +0000

    Completed problem 1

---
+++ jan01.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        
+        int i = 0, j = 0;
+        int n = g.size(), m = s.size();
+        while (i < n && j < m) {
+            if (g[i] <= s[j]) i++, j++;
+            else j++;
+        }
+        
+        return i;
+    }
+};
