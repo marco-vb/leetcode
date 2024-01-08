699ddc6f3349171fa2ca9765ddcd3388815d5534
marco-vb <marcofilipe03@hotmail.com>
2024-01-08 16:53:52 +0000

    Completed problem 3

---
+++ jan03.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numberOfBeams(vector<string>& bank) {
+        vector<string> san;
+        
+        for (auto& s : bank) {
+            if (count(s.begin(), s.end(), '1') == 0) continue;
+            san.push_back(s);
+        }
+        
+        int n = san.size();
+        int ans = 0;
+        for (int i = 1; i < n; i++) {
+            auto& s = san[i];
+            auto& b = san[i-1];
+            int t = count(s.begin(), s.end(), '1');
+            int bt = count(b.begin(), b.end(), '1');
+            ans += bt * t;
+        }
+        
+        return ans;
+    }
+};
