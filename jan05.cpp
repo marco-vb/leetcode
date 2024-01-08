68a20f1e33e72566df4cd0d9c202e54a124c4f8c
marco-vb <marcofilipe03@hotmail.com>
2024-01-08 17:25:22 +0000

    Completed problem 5

---
+++ jan05.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size(), k = 0;
+        vector<int> curr;
+        
+        for (int i = 0; i < n; i++) {
+            int idx = lower_bound(curr.begin(), curr.end(), nums[i]) - curr.begin();
+            
+            if (idx == k) curr.push_back(nums[i]), k++;
+            else curr[idx] = nums[i];
+        }
+        
+        return k;
+    }
+};
